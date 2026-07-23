62e8ce9c77664c74a4849676e9fe65bc8e2f0ca1
suat0 <suat.karabicak@sabanciuniv.edu>
2026-07-24 00:11:37 +0300

    Wire BallController to Ball object and fix scoring

--- Assets/_Project/Scripts/Gameplay/PlayerController.cs
+++ Assets/_Project/Scripts/Gameplay/PlayerController.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
+    
 
     void Update()
     {
 
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         transform.position += new Vector3(horizontalInput * speed * Time.deltaTime, 0, 0);
+        
     }
 }
